70226da7358f57e499b9932e6d4ecafcb19a9319
Jorchis <92830934+Jorchis@users.noreply.github.com>
2023-11-16 16:55:08 -0600

    Update main.py

--- QKP/main.py
+++ QKP/main.py
@@ -5,17 +5,25 @@ import string
 import time
 from Calculate import *
 
+#Open the file with the instance´s name and optimal value, and assign ´the opened file´ to Instances_data
 Instances_data=open("Instances data.txt", "r")
 
+#Open the file that will contain the results obtained, and assign that ´opened file´ to Results_file
 Results_file=open("Results.txt", "w")
-#Erase the contents of Results_file
+#Erase the contents of Results_file, so we can overwrite its contents with the most recent results
 Results_file.write("")
+#Close the file
+Results_file.close()
 
+#Reopen the file, but now in ´append mode´, so we can just append each new result without overwriting any previous contents
 Results_file=open("Results.txt", "a")
 
 i=0
 
-"""while not Instances_data.readline()=="EOF":
+"""
+#while (the next line read in the file with the instances´ data is not equal to "EOF", that stands for "End Of File")
+while not Instances_data.readline()=="EOF":
+    #do
     if(i%2==0):
         Optimal=Instances_data.readline().replace("\n", "")
         Instance_name=Instances_data.readline().replace("\n", "")
@@ -27,6 +35,7 @@ i=0
     Results_file.write(Calculate(Path,Optimal)+"\n")
     i=i+1"""
 
+#
 Path="./Instances/Prueba.txt"
 Optimal="9566"
 print(Calculate(Path,Optimal),"\n")
